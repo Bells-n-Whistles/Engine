66a7fe7fac1c0ea08d0d2e7935d4fbc887d7f3d0
55AAh <kostya.kulik@gmail.com>
2020-09-01 18:30:57 +0300

    Sprite isometry test

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,47 @@ use sfml::{
     graphics::{self, RenderTarget, Transformable},
     system, window,
 };
+use system::Vector2f;
 
 fn main() {
     let font = graphics::Font::from_file(&check_resource("resources\\sansation.ttf")).unwrap();
+    let grass_texture = graphics::Texture::from_file(&check_resource("resources\\grass.jpg")).unwrap();
+    let stone_texture = graphics::Texture::from_file(&check_resource("resources\\stone.jpg")).unwrap();
 
     let mut window = graphics::RenderWindow::new(
         (800, 600),
-        "SFML test - mouse & text",
+        "SFML test - sprites",
         window::Style::CLOSE,
         &Default::default(),
     );
     window.set_mouse_cursor_visible(false);
     window.set_framerate_limit(60);
 
+    let default_view = window.default_view().to_owned();
+    let mut iso_view = default_view.to_owned();
+    iso_view.set_size(Vector2f::new(default_view.size().x, default_view.size().y * 2.0));
+    iso_view.set_center(default_view.size() * 0.5);
+
     let mut circle = graphics::CircleShape::new(4., 30);
     let mut texts: Vec<graphics::Text> = Vec::new();
     let mut mp_text = graphics::Text::new("", &font, 14);
     let mut is_cursor_visible = false;
     let mut is_grabbed = false;
+
+    let sprite_size = 256.0;
+    let sprite_scale = 0.05;
+    let sprite_scaled_size = sprite_size * sprite_scale;
+    let sprite_iso_size = sprite_scaled_size * 2f32.sqrt();
+    let mut grass_sprite = graphics::Sprite::new();
+    grass_sprite.set_texture(&grass_texture, true);
+    grass_sprite.set_origin(Vector2f::new(sprite_size / 2.0, sprite_size / 2.0));
+    grass_sprite.set_scale(Vector2f::new(sprite_scale, sprite_scale));
+    grass_sprite.rotate(45.0);
+    let mut stone_sprite = graphics::Sprite::new();
+    stone_sprite.set_texture(&stone_texture, true);
+    stone_sprite.set_origin(Vector2f::new(sprite_size / 2.0, sprite_size / 2.0));
+    stone_sprite.set_scale(Vector2f::new(sprite_scale, sprite_scale));
+    stone_sprite.rotate(45.0);
 
     macro_rules! add_text {
         ($x:expr, $y:expr, $fmt:expr, $($arg:tt)*) => {
@@ -65,6 +88,44 @@ fn main() {
             }
         }
 
+        window.clear(graphics::Color::BLACK);
+
+        let art = [
+            "                                                                                       ",
+            "                                                                                       ",
+            "                                                                                       ",
+            "   #   #   #####   #       #        ###        #   #    ###    ####    #       ####    ",
+            "   #   #   #       #       #       #   #       #   #   #   #   #   #   #       #   #   ",
+            "   #####   #####   #       #       #   #       # # #   #   #   ####    #       #   #   ",
+            "   #   #   #       #       #       #   #       # # #   #   #   #  #    #       #   #   ",
+            "   #   #   #####   #####   #####    ###         # #     ###    #   #   #####   ####    ",
+            "                                                                                       ",
+            "                                                                                       ",
+            "                                                                                       ",
+        ];
+
+        window.set_view(&iso_view);
+        macro_rules! art2iso {
+            ($x: expr, $y: expr) => {
+                cart2iso(Vector2f::new(($x as f32 - 43.5) * sprite_iso_size / 2.0 , (5.5 - $y as f32) * sprite_iso_size / 2.0))
+            }
+        }
+        for (y, row) in art.iter().enumerate() {
+            for (x, char) in row.chars().enumerate() {
+                match char {
+                    '#' => {
+                        stone_sprite.set_position(art2iso!(x, y));
+                        window.draw(&stone_sprite);
+                    }
+                    _ => {
+                        grass_sprite.set_position(art2iso!(x, y));
+                        window.draw(&grass_sprite);
+                    },
+                }
+            }
+        }
+        window.set_view(&default_view);
+
         let mp = window.mouse_position();
         let dmp = window::mouse::desktop_position();
         let cur_vis_msg = if is_cursor_visible {
@@ -85,8 +146,6 @@ fn main() {
         ));
 
         circle.set_position((mp.x as f32, mp.y as f32));
-
-        window.clear(graphics::Color::BLACK);
 
         // Move texts up so they won't overlap
         for i in (0..texts.len()).rev() {
@@ -112,9 +171,15 @@ fn main() {
         if !is_cursor_visible {
             window.draw(&circle);
         }
-        window.draw(&mp_text);
+
+        window.draw(&mp_text);        
+
         window.display();
     }
+}
+
+fn cart2iso(position: Vector2f) -> Vector2f {
+    Vector2f::new(400.0 + position.x - position.y, 300.0 - position.x - position.y)
 }
 
 // Convert relative file paths to absolute
